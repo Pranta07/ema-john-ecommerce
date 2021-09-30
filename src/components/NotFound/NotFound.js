a4183710ffb25faf45d8794e64bdb182bbd795f0
Pranta07 <palpranta@gmail.com>
2021-10-01 01:40:29 +0600

    route setup done

---
+++ src/components/NotFound/NotFound.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const NotFound = () => {
+    return (
+        <div>
+            <h1>404</h1>
+            <h3>Not Found!</h3>
+            <p>The requested resources could not be found on this server!</p>
+        </div>
+    );
+};
+
+export default NotFound;
